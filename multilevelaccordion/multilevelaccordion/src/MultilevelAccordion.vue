31894c11c24b28e279d26a6ea17125c7352dc963
santiago <stgoddv@gmail.com>
2019-12-29 17:17:32 -0300

    added configuration for component npm encapsulation

---
+++ multilevelaccordion/multilevelaccordion/src/MultilevelAccordion.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="accordion-root">
+    <ul class="list-root">
+      <multilevel-accordion-children
+        v-for="(child, index) in tree.children"
+        :key="index"
+        :tree="child"
+        :position="index"
+        :interleaveOffset="1"
+        :reference="`${index}`"
+        :level="0"
+        :marginLeft="marginLeft"
+      >
+        <template slot-scope="_">
+          <slot
+            :tree="_.tree"
+            :interleaved="_.interleaved"
+            :expanded="_.expanded"
+            :level="_.level"
+          ></slot>
+        </template>
+      </multilevel-accordion-children>
+    </ul>
+  </div>
+</template>
+
+<script>
+import MultilevelAccordionChildren from "./MultilevelAccordionChildren";
+
+export default {
+  props: {
+    tree: {
+      type: Object
+    },
+    marginLeft: {
+      type: Number,
+      default: 0
+    }
+  },
+  components: {
+    MultilevelAccordionChildren
+  }
+};
+</script>
